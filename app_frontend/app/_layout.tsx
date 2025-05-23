cbcc274c3d0329b16ebc3db1165fe2f9bdf684ea
mxber2022 <mxber2022@gmail.com>
2025-05-23 16:42:44 +0800

    `import.meta` is not supported  fix

--- app_frontend/app/_layout.tsx
+++ app_frontend/app/_layout.tsx
@@ -1,3 +1,16 @@
+if (typeof document === 'undefined') {
+  (global as any).document = {};
+}
+import '@walletconnect/react-native-compat';
+import { WagmiProvider } from 'wagmi';
+import { mainnet, polygon, arbitrum } from '@wagmi/core/chains';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import {
+  createAppKit,
+  defaultWagmiConfig,
+  AppKit,
+} from '@reown/appkit-wagmi-react-native';
+
 import { useEffect } from 'react';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
@@ -5,9 +18,37 @@ import { useFrameworkReady } from '@/hooks/useFrameworkReady';
 import { SplashScreen } from 'expo-router';
 import { useFonts } from 'expo-font';
 import { Inter_400Regular, Inter_700Bold } from '@expo-google-fonts/inter';
-
 // Prevent splash screen from auto-hiding
 SplashScreen.preventAutoHideAsync();
+
+const queryClient = new QueryClient();
+
+// 1. Get projectId at https://cloud.reown.com
+const projectId = '210025460ad550f8aa621ee6a7f5e2d7';
+
+// 2. Create config
+const metadata = {
+  name: 'AppKit RN',
+  description: 'AppKit RN Example',
+  url: 'https://reown.com/appkit',
+  icons: ['https://avatars.githubusercontent.com/u/179229932'],
+  redirect: {
+    native: 'YOUR_APP_SCHEME://',
+    universal: 'YOUR_APP_UNIVERSAL_LINK.com',
+  },
+};
+
+const chains = [mainnet, polygon, arbitrum] as const;
+
+const wagmiConfig = defaultWagmiConfig({ chains, projectId, metadata });
+
+// 3. Create modal
+createAppKit({
+  projectId,
+  wagmiConfig,
+  defaultChain: mainnet, // Optional
+  enableAnalytics: true, // Optional - defaults to your Cloud configuration
+});
 
 export default function RootLayout() {
   useFrameworkReady();
@@ -31,11 +72,15 @@ export default function RootLayout() {
 
   return (
     <>
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
+      <WagmiProvider config={wagmiConfig}>
+        <QueryClientProvider client={queryClient}>
+          <Stack screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+            <Stack.Screen name="+not-found" />
+          </Stack>
+          <StatusBar style="auto" />
+        </QueryClientProvider>
+      </WagmiProvider>
     </>
   );
 }
